af943af9b884d6fbee682991c2a367f5dee9d61a
lolagarciamoreno2011 <129238602+lolagarciamoreno2011@users.noreply.github.com>
2023-06-13 17:28:30 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,20 @@
+document.getElementById('menu').addEventListener('click', function()
+{
+
+document.getElementById('navega').classList.toggle('mostrar');
+
+
+});
+
+
+function reproducir(){
+document.getElementById('miVideo').play();
+
+}
+
+
+function pausar(){
+    document.getElementById('miVideo').pause();
+
+
+}
---
+++ js/codigoLightbox.js
@@ -0,0 +1,6 @@
+    lightbox.option({
+      'wrapAround': true,
+      'albumLabel':"Imagen %1 de %2",
+      'fadeDuration':1000,
+     
+    })
